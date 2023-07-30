9ec5316106671b27024f9bad1d0646a9449b534b
xcode <xcoderole@qq.com>
2023-07-30 21:58:02 +0800

    2023-7-30 dp

--- dp/139.单词拆分.cpp
+++ dp/139.单词拆分.cpp
@@ -6,10 +6,27 @@
 
 // @lc code=start
 class Solution {
+vector<int> memo;
 public:
     bool wordBreak(string s, vector<string>& wordDict) {
-
+        memo=vector<int>(s.size(),-1);
+        return dp(s,0,wordDict);
     }
+    bool dp(string s,int i, vector<string>& wordDict){
+        if(i==s.size()) return true;
+        if(memo[i]!=-1) return memo[i];
+        bool isPrefix=false;
+        for(string t:wordDict){
+            if(s.substr(i,t.size())==t){
+                isPrefix = isPrefix || dp(s,i+t.size(),wordDict);
+            }
+        }
+        memo[i]=isPrefix?1:0;
+        return isPrefix;
+    }
+    // bool isPrefix(string s,int i,string t){
+    //     return s.substr(i,t.size())==t;
+    // }
 };
 // @lc code=end
 
